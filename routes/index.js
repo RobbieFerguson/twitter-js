9a51cdd82dcf015187cddf68f9eaa34fb427f856
Tremayne <trey@Trey-MBP.local>
2015-04-29 18:27:04 -0400

    more sock

--- routes/index.js
+++ routes/index.js
@@ -8,38 +8,37 @@ var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
-
-
-router.get('/', function (req, res) {
-  var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true} );
-});
-
-router.get('/users/:name/:id', function(req, res) {
-	var id = req.params.id;
-	var name = req.params.name
-	var unique = tweetBank.find( {id: id} );
-	res.render( 'index', {title: 'Posted by'+name, tweets: unique} );
-});
-
-router.get('/users/:name', function(req, res) {
-  var name = req.params.name;
-  var list = tweetBank.find( {name: name} );
-  console.log(name);
-  
-  res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list, showForm: true, namePage: true} );
-});
-
-router.post('/submit', urlencodedParser, function(req, res) {
-  var name = req.body.name;
-  var text = req.body.text;
-  
-  io.sockets.emit('new_tweet', tweetBank.add(name, text));
-
-  //tweetBank.add(name, text);
-  res.redirect('/');
-});
-
 module.exports = function(io){
+	router.get('/', function (req, res) {
+	  var tweets = tweetBank.list();
+	  res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true} );
+	});
+
+	router.get('/users/:name/:id', function(req, res) {
+		var id = req.params.id;
+		var name = req.params.name
+		var unique = tweetBank.find( {id: id} );
+		res.render( 'index', {title: 'Posted by'+name, tweets: unique} );
+	});
+
+	router.get('/users/:name', function(req, res) {
+	  var name = req.params.name;
+	  var list = tweetBank.find( {name: name} );
+	  console.log(name);
+	  
+	  res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list, showForm: true, namePage: true} );
+	});
+
+	router.post('/submit', urlencodedParser, function(req, res) {
+	  var name = req.body.name;
+	  var text = req.body.text;
+	  
+	  console.log(tweetBank.list());
+	  io.sockets.emit('new_tweet', tweetBank[tweetBank.length-1]);
+
+	  //tweetBank.add(name, text);
+	  //res.redirect('/');
+	});
+
 	return router;
 };
